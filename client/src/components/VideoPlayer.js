c1a734b9c16c406555aa5eea7dc1bd95c3f19476
Akhil <108akhilraj@gmail.com>
2023-09-10 21:36:55 +0530

    videoPlayer modifications

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React ,{useContext} from 'react'
 import {Grid,Typography,Paper} from '@mui/material'
 
-function VideoPlayer() {
-  return (
-    <Grid>
+import { SocketContext } from '../Context';
 
-      
+function VideoPlayer() {
+
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
+  return (
+    <Grid container >
+      {stream && (
+        <Paper>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
+            <video playsInline muted ref={myVideo} autoPlay/>
+          </Grid>
+        </Paper>
+      )}
+      {callAccepted && !callEnded && (
+        <Paper >
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
+            <video playsInline ref={userVideo} autoPlay/>
+          </Grid>
+        </Paper>
+      )}
     </Grid>
-  )
+  );
 }
 
 export default VideoPlayer
